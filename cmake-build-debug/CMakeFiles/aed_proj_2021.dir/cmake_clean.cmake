ca52ad23cca4f9b583836b86c2295c142a12599c
toni-santos <a.oliveirasantos122@gmail.com>
2021-11-25 01:35:30 +0000

    setup commit

---
+++ cmake-build-debug/CMakeFiles/aed_proj_2021.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aed_proj_2021.dir/main.cpp.obj"
+  "aed_proj_2021.exe"
+  "aed_proj_2021.exe.manifest"
+  "aed_proj_2021.pdb"
+  "libaed_proj_2021.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aed_proj_2021.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
